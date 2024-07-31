3d5e771ee19d257cf3e946145d49b26b312fd3a8
User <User@WRSDLD309-040aa.dut.ac.za>
2024-07-31 16:01:51 +0200

    comitted

--- src/components/like.jsx
+++ src/components/like.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
 import { CiHeart } from "react-icons/ci";
 
+let iconColor = "red"
+
+
 function Icon(){
+const [isLiked, setisLiked] = useState(false)
+
+    function ChangeLike(){
+        
+
+        if(isLiked){
+            setisLiked(false)
+            iconColor = "red"
+            console.log("Liked!")
+        }
+
+        if(!isLiked){
+            setisLiked(true)
+            iconColor = "white"
+            console.log("Unliked!")
+        }
+    }
+
     return(
-        <CiHeart id="heart" />
+        <CiHeart color={iconColor} size={30} onClick={ChangeLike} />
     )
 }
 export default Icon
--- src/components/main card.jsx
+++ src/components/main card.jsx
@@ -1,20 +1,28 @@
 import { MdOutlineBedroomParent } from "react-icons/md";
 import { LiaHotTubSolid } from "react-icons/lia";
+import { CiHeart } from "react-icons/ci";
+import Icon from "./like";
 
 function Div(){
+  
 
     return(
         <div className="div1">
-            <div className='div2' style={{backgroundImage:`url(${require("../assets/house.jpg")})`}}></div>
+            <div className='div2' style={{backgroundImage:`url(${require("../assets/house.jpg")})`}}>
+                <Icon/>
+            </div>
+
             <div className='div3'>
                 <h4 style={{color:"gray"}}>Spanish Villa - 3 Y Old</h4><br></br>
                 <h1>$1.5 Million</h1><br></br>
                 <p style={{color:"gray"}}>86 Armitage Drive</p>
             </div>
+
             <div className='div4'>
             <MdOutlineBedroomParent id="bedroom"/><h3>3</h3><h3 style={{color:"gray"}}>Bedrooms</h3><br></br>
             <LiaHotTubSolid id="tub"/><h3>2</h3><h3 style={{color:"gray"}}>Bathrooms</h3>
             </div>
+
             <div className='div5'>
                 <p style={{color:"gray"}}>REALATOR</p><br></br>
                 <section className="info">
