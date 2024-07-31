7d763dd2b02dd7c5ebe64273a9a768a24ca8f60b
User <User@WRSDLD309-040aa.dut.ac.za>
2024-07-31 12:08:54 +0200

    icons added

--- src/components/main card.jsx
+++ src/components/main card.jsx
@@ -1,12 +1,18 @@
-
+import { MdOutlineBedroomParent } from "react-icons/md";
 
 function Div(){
 
     return(
         <div className="div1">
             <div className='div2' style={{backgroundImage:`url(${require("../assets/house.jpg")})`}}></div>
-            <div className='div3'>g</div>
-            <div className='div4'>h</div>
+            <div className='div3'>
+                <h4 style={{color:"gray"}}>Spanish Villa - 3 Y Old</h4><br></br>
+                <h1>$1.5 Million</h1><br></br>
+                <p style={{color:"gray"}}>86 Armitage Drive</p>
+            </div>
+            <div className='div4'>
+            <MdOutlineBedroomParent id="bedroom"/><h3>3</h3><h3 style={{color:"gray"}}>Bedrooms</h3><br></br>
+            </div>
             <div className='div5'>dd</div>
         </div>
     )
