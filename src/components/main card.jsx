90d553de5b883bd1e58407e8812d7c4947417167
User <User@WRSDLD309-040aa.dut.ac.za>
2024-07-31 09:22:32 +0200

    componetnts added

---
+++ src/components/main card.jsx
@@ -0,0 +1,11 @@
+function Div(){
+    return(
+        <div className="div1">
+            <div className='div2'>f</div>
+            <div className='div3'>g</div>
+            <div className='div4'>h</div>
+            <div className='div5'>dd</div>
+        </div>
+    )
+}
+export default Div
