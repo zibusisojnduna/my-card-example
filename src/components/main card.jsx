543bb95570baeeee5d3234d0fb9103959b730d0c
User <User@WRSDLD309-040aa.dut.ac.za>
2024-07-31 09:54:32 +0200

    pic added

--- src/components/main card.jsx
+++ src/components/main card.jsx
@@ -1,7 +1,7 @@
 function Div(){
     return(
         <div className="div1">
-            <div className='div2'>f</div>
+            <div className='div2'></div>
             <div className='div3'>g</div>
             <div className='div4'>h</div>
             <div className='div5'>dd</div>
