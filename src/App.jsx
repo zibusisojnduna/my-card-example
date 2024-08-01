9faa62952ad2b38fb847e8daa3116a5c74a98a23
User <User@WRSDLD309-040aa.dut.ac.za>
2024-08-01 12:52:12 +0200

    houses added

--- src/App.jsx
+++ src/App.jsx
@@ -25,21 +25,76 @@ let house = {
   addressOfHouse : "199 Margerita Avenue",
   numberOfBedrooms : 4,
   numberOfBathrooms : 3,
-  nameOfRealtor : "Joel Spoletti",
+  nameOfRealtor : "Daniella Spoletti",
   numberOfRealtor: "+39 63 786 4509",
 }
 
+let housesForSale = [
+  {
+  id : 1,
+  image:"./src/assets/beach.jpg",
+  typeOfHouse : "Beachside Bungalo",
+  ageOfHouse : 8,
+  priceOfHouse : "$525,000",
+  addressOfHouse : "12 Coral Street",
+  numberOfBedrooms : 2,
+  numberOfBathrooms : 1,
+  nameOfRealtor : "Georgina Johnson",
+  numberOfRealtor: "+63 69 125 9234",
+
+  },
+
+  {
+  id : 2,
+  image:"./src/assets/apartment.jpg",
+  typeOfHouse : "Uptown Apartment",
+  ageOfHouse : 2,
+  priceOfHouse : "$4000 per month",
+  addressOfHouse : "No. 8, 45 Blossom Street",
+  numberOfBedrooms : 2,
+  numberOfBathrooms : 2,
+  nameOfRealtor : "Eleanor Young",
+  numberOfRealtor: "+23 78 458 0091",
+  },
+
+  {
+  id : 3,
+  image:"./src/assets/townhouse.jpg",
+  typeOfHouse : "Stone Townhouse",
+  ageOfHouse : 32,
+  priceOfHouse : "$450,000",
+  addressOfHouse : "8 Clemson Drive",
+  numberOfBedrooms : 3,
+  numberOfBathrooms : 2,
+  nameOfRealtor : "Isabell West",
+  numberOfRealtor: "+24 33 109 6712",
+  }
+]
 
 
   // localStorage.setItem();
 
   return (
     <>
-      <Div>
+    <section id="main">
+      <Div accommodation={house}/>
+      <Div accommodation={housesForSale[0]}/>
+      <Div accommodation={housesForSale[1]}/>
+      <Div accommodation={housesForSale[2]}/>
+    </section>
+      
+      
         
-      </Div>
+      
+{/* 
+      <div style={{display:"flex"}}>
+        {housesForSale.map((items, index) => (
+          <Div name={items} key={index}/>
+        ))}
+      </div> */}
 
-     
+
+  
     </>
   )
 }
--- src/components/main card.jsx
+++ src/components/main card.jsx
@@ -3,31 +3,32 @@ import { LiaHotTubSolid } from "react-icons/lia";
 import { CiHeart } from "react-icons/ci";
 import Icon from "./like";
 
-function Div(){
-  
+function Div({accommodation}){
+  console.log(accommodation)
 
     return(
-        <div className="div1">
-            <div className='div2' style={{backgroundImage:`url(${require("../assets/house.jpg")})`}}>
+        
+            <div className="div1">
+            <div className='div2' style={{backgroundImage:`url(${require(accommodation.image)})`}}>
                 <Icon/>
             </div>
 
             <div className='div3'>
-                <h4 style={{color:"gray"}}>Spanish Villa - 3 Y Old</h4><br></br>
-                <h1>$1.5 Million</h1><br></br>
-                <p style={{color:"gray"}}>86 Armitage Drive</p>
+                <h4 style={{color:"gray"}}>{accommodation.typeOfHouse}- {accommodation.ageOfHouse} Y Old</h4><br></br>
+                <h1>{accommodation.priceOfHouse}</h1><br></br>
+                <p style={{color:"gray"}}>{accommodation.addressOfHouse}</p>
             </div>
 
             <div className='div4'>
-            <MdOutlineBedroomParent id="bedroom"/><h3>3</h3><h3 style={{color:"gray"}}>Bedrooms</h3><br></br>
-            <LiaHotTubSolid id="tub"/><h3>2</h3><h3 style={{color:"gray"}}>Bathrooms</h3>
+            <MdOutlineBedroomParent id="bedroom"/><h3>{accommodation.numberOfBedrooms}</h3><h3 style={{color:"gray"}}>Bedrooms</h3><br></br>
+            <LiaHotTubSolid id="tub"/><h3>{accommodation.numberOfBathrooms}</h3><h3 style={{color:"gray"}}>Bathrooms</h3>
             </div>
 
             <div className='div5'>
                 <p style={{color:"gray"}}>REALATOR</p><br></br>
                 <section className="info">
                     <img id="profile" src=".\src\assets\profile.jpg"></img>
-                    <h1>Dana Smith<br></br><p style={{color:"gray", fontSize:"small"}}>(+27) 67 845-2345</p></h1>
+                    <h1>{accommodation.nameOfRealtor}<br></br><p style={{color:"gray", fontSize:"small"}}>{accommodation.numberOfRealtor}</p></h1>
                     
                     
                 
@@ -35,6 +36,8 @@ function Div(){
                 
             </div>
         </div>
+        
+        
     )
 }
 export default Div
