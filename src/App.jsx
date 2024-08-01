da8d4fad89fd71fe9bdda86fa1f4f4be58e63765
User <User@WRSDLD309-040aa.dut.ac.za>
2024-08-01 09:34:28 +0200

    local storage

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Div from './components/main card'
 
-
 function App() {
   const [count, setCount] = useState(0)
+
+const game ={
+  title : "Uncharted 2:Among Thieves",
+  year : 2010,
+  system : "Playstation 3",
+  modes : ["Single Player", "LAN", "Multiplayer"],
+  price : 29.99,
+  available : true,
+}
+
+  localStorage.setItem(game, {});
 
   return (
     <>
